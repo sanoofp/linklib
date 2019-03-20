967db847b9a9310e6cba44d02595e56c5cff22c3
sanoofp <sanoofpb24@gmail.com>
2019-03-20 16:33:20 +0530

    Added heroku-postbiuld

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", require("./routes/api"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => console.log("Server Started."));
