ccc508f32d49bb5e4938910c27e405736c1539b3
sanoofp <sanoofpb24@gmail.com>
2019-03-23 01:45:14 +0530

    Added compression

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const compression = require('compression');
 const path = require("path");
 const apiRoutes = require("./routes/api");
 
 const app = express();
 
-app.use(express.json());
 app.set("port", process.env.PORT || 5000);
+
+app.use(express.json());
+app.use(compression());
 
 app.use("/api", apiRoutes);
 
