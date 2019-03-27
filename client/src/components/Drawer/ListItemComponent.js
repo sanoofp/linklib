40a97c041909ffaf26430ead4a0cc181e19af3a2
sanoofp <sanoofpb24@gmail.com>
2019-03-27 13:01:49 +0530

    [Error] Fixed toggleDarkThemeAndDrawer export error

--- client/src/components/Drawer/ListItemComponent.js
+++ client/src/components/Drawer/ListItemComponent.js
@@ -6,7 +6,6 @@ import {
   toggleDrawer,
   dialogAction
 } from "../../actions/appStateAction";
-import { toggleDarkThemeAndDrawer } from "../../actions/appStateAction";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
