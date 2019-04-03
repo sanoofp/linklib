e1d407a1d0a0b89dfc75f22ab3e60e9a7bf71f80
sanoofp <sanoofpb24@gmail.com>
2019-04-03 23:23:24 +0530

    [Heroku] Fix web-push

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const compression = require("compression");
 const path = require("path");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const webpush = require("web-push");
 
-const { mongoURI, vapidPrivate, vapidPublic } = require("./config/keys");
+const { mongoURI } = require("./config/keys");
 
 const app = express();
 
@@ -19,20 +18,6 @@ mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log("Connected to Database"))
   .catch(err => console.log("Database connection failed ", err));
-
-webpush.setVapidDetails("mailto:sanoofp7@gmail.com", vapidPublic, vapidPrivate);
-
-// Webpush subscribe
-app.post("/subscribe", (req, res) => {
-  const subscription = req.body;
-  console.log(subscription);
-
-  res.status(201).json({});
-  const payload = JSON.stringify({ title: "Push test" });
-  webpush
-    .sendNotification(subscription, payload)
-    .catch(err => console.error(err));
-});
 
 app.use("/api/user", require("./routes/api/user"));
 app.use("/api/link", require("./routes/api/link"));
