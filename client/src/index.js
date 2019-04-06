e435761dcb15a320fc7d4ec518010979e44cd62c
sanoofp <sanoofpb24@gmail.com>
2019-04-07 02:15:52 +0530

    [PWA] Add custom service worker - test 5

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,6 @@ import { Provider } from "react-redux";
 import store from "./Store";
 import App from "./App";
 
-if("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js")
-      .then(reg => console.log("REGISTERED"))
-      .catch(err => console.log("SW NOT REGISTRED", err));
-  });
-}
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
