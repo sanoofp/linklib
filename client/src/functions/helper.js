e177e98fa6e2f48eefef1ceb539be4e2353c8c2a
sanoofp <sanoofpb24@gmail.com>
2019-04-19 16:55:18 +0530

    [Function] Modified the url validation function in dashboard component

--- client/src/functions/helper.js
+++ client/src/functions/helper.js
@@ -47,7 +47,7 @@ export const truncateStringTo = (str, upto) => {
 }
 
 export const validateURL = url => {
-  const regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
-  const regex = new RegExp(regexQuery, "i")
+  const regexQuery = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+  const regex = new RegExp(regexQuery, "i");
   return regex.test(url);
 }
