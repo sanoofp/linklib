a00d8cc2ea635c1676a138cb45aa67a7e5ce0733
sanoofp <sanoofpb24@gmail.com>
2019-04-27 01:59:27 +0530

    [UI] Add svg arts in search component

--- client/src/components/SearchMain/index.js
+++ client/src/components/SearchMain/index.js
@@ -3,21 +3,26 @@ import { connect } from "react-redux";
 import SearchBox from "../HelperComponent/search/SearchBox";
 import { SearchMainContainer } from "./styles";
 import ResultItem from "../HelperComponent/search/ResultItem";
+import ArtLight from "./art_light.svg";
+import ArtDark from "./art_dark.svg";
 
 class SearchMain extends Component {
   render() {
+    const { darkTheme } = this.props;
     const list = this.props.globalSearchResult;
     let list_view;
     if(list.length > 0) {
       list_view = list.map((result, i) => <ResultItem isMain={true} result={result} key={i} />);
     } else {
-      list_view = <p className="no-result">No Result</p>;
+      list_view = <div className="no-result">
+        <img src={darkTheme ? ArtDark : ArtLight} alt="Search Art" />
+        <p>No Link Found</p>
+      </div>;
     }
     return <SearchMainContainer>
       <div className="container">
         <div className="row">
           <div className="col-md-10 py-3 mx-auto">
-            <h1>Search</h1>
             <SearchBox isMain={true} />
             {list_view}
           </div>
@@ -29,6 +34,7 @@ class SearchMain extends Component {
 
 const mapStateToProps = state => ({
   globalSearchResult: state.linkReducer.globalSearchResult,
+  darkTheme: state.appStateReducer.darkTheme
 });
 
 export default connect(mapStateToProps)(SearchMain);
--- client/src/components/SearchMain/styles.js
+++ client/src/components/SearchMain/styles.js
@@ -1,20 +1,27 @@
 import styled from "styled-components";
 
 export const SearchMainContainer = styled.div`
-  text-align: center;
-  h1 {
-    font-size: 2.5em;
-  }
   .no-result {
-    font-size: 1.3em;
     margin: 20px 0;
+    text-align: center;
+    width: 100%;
+    p {
+      font-size: 1.67em;
+      margin: 5px 0;
+    }
+    img {
+      margin: 12px 0;
+      width: 50%;
+    }
   }
   @media (max-width: 580px) {
     input {
       margin: 6px 0;
     }
-    h1{
-      font-size: 1.4em;
+    .no-result {
+      img {
+        width: 100%;
+      }
     }
   }
 `
